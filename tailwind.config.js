c7a74c0983a0d3cd5b2266f66d15d1f589a3ae4f
adi12345609876 <radinath098@gmail.com>
2024-06-02 22:28:43 +0530

    newest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,html}",
   ],
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {
       backgroundImage: {
